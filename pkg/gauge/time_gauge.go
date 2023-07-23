27870bdd88ab5452598bec0cebdbcc3eac474955
ryoji-ishii <goccha.dev@gmail.com>
2023-07-23 12:00:35 +0900

    refactoring

--- pkg/gauge/time_gauge.go
+++ pkg/gauge/time_gauge.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// New 指定期間の時間計測機を生成する
 func New(begin, end time.Time) *TimeGauge {
 	tg := &TimeGauge{
 		begin: begin,
@@ -39,13 +40,23 @@ func (t *TimeGauge) Duration() time.Duration {
 	}
 	return *t.duration
 }
+
+// Seconds returns the duration as a floating point number of seconds.
 func (t *TimeGauge) Seconds() float64 {
 	return t.Duration().Seconds()
 }
+
+// Minutes
+func (t *TimeGauge) Minutes() float64 {
+	return t.Duration().Minutes()
+}
+
+// Hours
 func (t *TimeGauge) Hours() float64 {
 	return t.Duration().Hours()
 }
 
+// Rounds
 func (t *TimeGauge) Rounds() (hour int, minute int, second int) {
 	d := t.Duration().Round(time.Second)
 	h := d / time.Hour
@@ -56,6 +67,7 @@ func (t *TimeGauge) Rounds() (hour int, minute int, second int) {
 	return int(h), int(m), int(s)
 }
 
+// RoundAll
 func (t *TimeGauge) RoundAll() (hour int, minute int, second int, milli int, micro int, nano int) {
 	d := t.Duration()
 	h := d / time.Hour
@@ -72,7 +84,8 @@ func (t *TimeGauge) RoundAll() (hour int, minute int, second int, milli int, mic
 	return int(h), int(m), int(s), int(ml), int(mi), int(n)
 }
 
-func (t TimeGauge) Format(s fmt.State, verb rune) {
+// Format
+func (t *TimeGauge) Format(s fmt.State, verb rune) {
 	switch verb {
 	case 'v':
 		_, _ = io.WriteString(s, t.Duration().String())
@@ -116,6 +129,8 @@ func (t TimeGauge) Format(s fmt.State, verb rune) {
 		_, _ = io.WriteString(s, fmt.Sprintf("%03d", n))
 	}
 }
+
+// Split 期間を基準時刻で分割する
 func (t *TimeGauge) Split(hour, min, sec, nsec int, loc *time.Location) []TimeGauge {
 	b := baseTime{
 		hour: hour,
@@ -124,12 +139,12 @@ func (t *TimeGauge) Split(hour, min, sec, nsec int, loc *time.Location) []TimeGa
 		nsec: nsec,
 		loc:  loc,
 	}
-	sleepTime := t.end.Sub(t.begin)
-	if sleepTime < 0 {
+	timeRange := t.end.Sub(t.begin)
+	if timeRange < 0 {
 		return []TimeGauge{}
 	}
 	m := make(map[string]*TimeGauge)
-	calc(b, t.begin, sleepTime, m)
+	calc(b, t.begin, timeRange, m)
 	times := make([]TimeGauge, 0, len(m))
 	for _, v := range m {
 		times = append(times, *v)
@@ -137,6 +152,7 @@ func (t *TimeGauge) Split(hour, min, sec, nsec int, loc *time.Location) []TimeGa
 	return times
 }
 
+// baseTime 基準時刻
 type baseTime struct {
 	hour int
 	min  int
@@ -145,23 +161,25 @@ type baseTime struct {
 	loc  *time.Location
 }
 
-func (t *baseTime) Time(tm time.Time) time.Time {
+// Time 指定日の基準時刻(time.Time)を返す
+func (t baseTime) Time(tm time.Time) time.Time {
 	return time.Date(tm.Year(), tm.Month(), tm.Day(), t.hour, t.min, t.sec, t.nsec, t.loc)
 }
 
-func calc(b baseTime, tm time.Time, sleepTime time.Duration, times map[string]*TimeGauge) {
-	if sleepTime <= 0 {
+// calc 開始日時からの経過時間を基準時刻で日付毎に分割する
+func calc(b baseTime, tm time.Time, timeRange time.Duration, times map[string]*TimeGauge) {
+	if timeRange <= 0 {
 		return
 	}
 	base := b.Time(tm)
 	diff := base.Sub(tm)
-	if diff > 0 { // 基準時刻よりも前に就寝した場合
+	if diff > 0 { // 開始が基準時刻よりも前
 		key := tm.Format("2006-01-02")
-		if sleepTime < diff { // 基準時刻よりも前に起床している場合
+		if timeRange < diff { // 基準時刻よりも前に終了している場合
 			times[key] = &TimeGauge{
 				date:  key,
 				begin: tm,
-				end:   tm.Add(sleepTime),
+				end:   tm.Add(timeRange),
 			}
 			return
 		} else {
@@ -170,26 +188,26 @@ func calc(b baseTime, tm time.Time, sleepTime time.Duration, times map[string]*T
 				begin: tm,
 				end:   base,
 			}
-			sleepTime -= diff
-			calc(b, base, sleepTime, times)
+			timeRange -= diff
+			calc(b, base, timeRange, times)
 			return
 		}
 	} else {
 		base = base.AddDate(0, 0, 1) // 基準日時を翌日にする
 		key := base.Format("2006-01-02")
-		diff := base.Sub(tm)  // 基準日時までの時間を算出
-		if sleepTime < diff { // 基準日時以前に起床している場合
+		diff = base.Sub(tm)   // 基準日時までの時間を算出
+		if timeRange < diff { // 基準日時以前に開始している場合
 			if v, ok := times[key]; ok {
-				v.end = tm.Add(sleepTime)
+				v.end = tm.Add(timeRange)
 			} else {
 				times[key] = &TimeGauge{
 					date:  key,
 					begin: tm,
-					end:   tm.Add(sleepTime),
+					end:   tm.Add(timeRange),
 				}
 			}
 			return
-		} else { // 基準日時以降に起床している場合
+		} else { // 基準日時以降に開始している場合
 			if v, ok := times[key]; ok {
 				v.end = base
 			} else {
@@ -199,13 +217,14 @@ func calc(b baseTime, tm time.Time, sleepTime time.Duration, times map[string]*T
 					end:   base,
 				}
 			}
-			sleepTime -= diff
-			calc(b, base, sleepTime, times)
+			timeRange -= diff
+			calc(b, base, timeRange, times)
 			return
 		}
 	}
 }
 
+// Overlap 指定した期間と重複しているかどうか
 func (t *TimeGauge) Overlap(start time.Time, end time.Time) bool {
 	if t.begin.Before(start) && t.end.After(start) {
 		return true
